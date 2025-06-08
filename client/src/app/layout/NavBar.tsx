7b01c62d1ab4f06c0d2426f3dd48142cc20fb110
Michael C. Herrera <161962992+michaelcherrera84@users.noreply.github.com>
2025-06-08 16:19:54 -0500

    End of Section 13

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -4,10 +4,13 @@ import { NavLink } from 'react-router';
 import MenuItemLink from '../shared/components/MenuItemLink.tsx';
 import { useStore } from '../../lib/stores/useStore.ts';
 import { Observer } from 'mobx-react-lite';
+import { useAccount } from '../../lib/hooks/useAccount.ts';
+import UserMenu from './UserMenu.tsx';
 
 export default function NavBar() {
 
     const { uiStore } = useStore();
+    const { currentUser } = useAccount();
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -29,9 +32,6 @@ export default function NavBar() {
                             <MenuItemLink to="/activities">
                                 Activities
                             </MenuItemLink>
-                            <MenuItemLink to="/createActivity">
-                                Create Activity
-                            </MenuItemLink>
                             <MenuItemLink to="/counter">
                                 Counter
                             </MenuItemLink>
@@ -39,9 +39,16 @@ export default function NavBar() {
                                 Errors
                             </MenuItemLink>
                         </Box>
-                        <MenuItem>
-                            User menu
-                        </MenuItem>
+                        <Box display="flex" alignItems="center">
+                            {currentUser ? (
+                               <UserMenu />
+                            ) : (
+                                <>
+                                    <MenuItemLink to="/login">Login</MenuItemLink>
+                                    <MenuItemLink to="/register">Register</MenuItemLink>
+                                </>
+                            )}
+                        </Box>
                     </Toolbar>
                 </Container>
                 <Observer>
